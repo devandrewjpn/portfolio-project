c1e84e78b2b149d17c5862dc8ee64a402d172bee
devandrewjpn <yuutoandrew.jpn@gmail.com>
2023-03-12 21:15:25 -0300

    starting..

---
+++ portfolio/src/Pages/First/First.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './First.module.css'
+import { ReactComponent as MainVector } from '../../Assets/Images/fundo.svg'
+
+const First = () => {
+  return (
+    <section className={styles.wrap}>
+      main
+    </section>
+  )
+}
+
+export default First
